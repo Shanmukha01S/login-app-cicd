9c61f88fa2213f7b8562ccdc2ccfa50e7ff3e303
Shanmukha RD <shanmukhas520@email.com>
2026-03-30 13:53:23 +0530

    Made some changes for login error

--- src/main/java/com/loginapp/LoginServlet.java
+++ src/main/java/com/loginapp/LoginServlet.java
@@ -1,14 +1,14 @@
 package com.loginapp;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, 
-                          HttpServletResponse response) 
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
                           throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
